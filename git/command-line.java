6adba60889641e1d4438f2476d8dff7090b9954f
s4562788 <ocaneric@colpatria.com>
2019-06-02 18:26:29 -0500

    Adjust command line file

--- git/command-line.java
+++ git/command-line.java
@@ -4,16 +4,18 @@ git checkout -b feature/ONEBANK-1478
 
 _______________________________________________________
 // tmp files - ignore
-git stash
-git stash list
-git stash apply // back to branch
+git stash 
+git stash save // which enables including temporary commit message, which will help you identify changes, among with other options
+git stash list // which lists all previously stashed commits (yes, there can be more) that were not poped
+git stash pop // which redoes previously stashed changes and removes them from stashed list
+git stash apply // which redoes previously stashed changes, but keeps them on stashed list
 
 _______________________________________________________
-// Undo a git add - remove files staged for a git commit
+// Undo a git ADD - remove files staged for a git commit
 git reset filename.txt
 
 _______________________________________________________
-// Undo working copy modifications of one file
+// Undo working copy MODIFICATOINS of one file
 git checkout -- file
 
 // You can also check out a particular version of a file:
@@ -25,3 +27,10 @@ git checkout HEAD -- file           # the version from the most recent commit
 git checkout HEAD^ -- file          # the version before the most recent commit
 
 _______________________________________________________
+
+// Sources
+
+- https://github.com/joshnh/Git-Commands
+- https://docs.gitlab.com/ee/topics/git/numerous_undo_possibilities_in_git/
+- https://git-scm.com/docs
+- https://danielkummer.github.io/git-flow-cheatsheet/
